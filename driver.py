dff1d64e5697d80252e32d3cb7c70853423372b5
Peter <peterfayez1285@gmail.com>
2023-05-15 21:04:04 +0300

    Add files via upload

---
+++ driver.py
@@ -0,0 +1,16 @@
+import csv
+from Algorithms.Miller_Rabin import miller_rabin
+from Algorithms.Fermat_Test import fermat
+
+
+def main():
+    raise NotImplementedError
+    # for i in range(1, 2000000):
+    #     if miller_rabin(i):
+    #         print(f"{i} is a miller prime\n")
+    #     if fermat(i):
+    #         print(f"{i} is a fermat prime\n")
+
+
+if __name__ == '__main__':
+    main()
